e7b7a39ab9c6efca0e657d409a9c2d57dc8c821b
soumns <2794122209@qq.com>
2022-03-03 09:57:59 +0800

    :zap: add-T-type

--- develop/T.ts
+++ develop/T.ts
@@ -2,7 +2,7 @@
  * 泛型
  */
 
-// 一、函数泛型
+// 一、泛型函数
 // T 代表Type,在定义泛型时通常用作第一个类型变量名称
 // K 表示对象中的键类型
 // V 表示对象中的值类型
@@ -16,8 +16,8 @@ type Keys = keyof typeof person; // 'name' | 'age'
 
 // 函数泛型 T 指代任意输入的类型, <T> 用来传递类型,链式传递给参数类型和返回类型使用
 function getKeys<T extends typeof person, K extends Keys>(
-  obj: T,
-  key: K
+    obj: T,
+    key: K
 ): T[K] {
   return obj[key];
 }
@@ -39,6 +39,17 @@ function getAge<T, M>(name: T, age: M): IFood<T, M> {
 }
 
 console.log(getAge('yoona', 19), '-->泛型接口');
+
+
+interface IGet {
+  <T>(money: T): T
+}
+
+let func: IGet = function <T>(money: T): T {
+  return money
+}
+
+console.log(func(100))
 
 // 三、泛型类
 interface IAnimal<T, M, K> {
@@ -63,21 +74,10 @@ let dog = new Animal('iu', 17, '骨头');
 console.log(dog.name, dog.age, '-->泛型类');
 console.log(dog.eat('骨头'));
 
-// 四、`
-interface IDes {
-  name: string;
-  age: number;
+// 四、泛型参数的默认类型
+function getT<T = string>(t: T): void {
+  console.log(t)
 }
 
-interface IDo {
-  name: string;
-  something: string;
-}
-
-function getSomething<T extends IDes | IDo>(object: T) {
-  return '';
-}
-
-getSomething({ name: 'iu', age: 17 });
 
 export {};
